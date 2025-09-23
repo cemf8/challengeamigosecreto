a19263748d2c3ef5d34715566d7466bb5a302ddc
cemf8 <claudiamezaflorian@gmail.com>
2025-09-23 18:20:01 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+const inputAmigo = document.getElementById("amigo");
+const listaAmigos = [];
+const ulListaAmigos = document.getElementById("listaAmigos");
+const ulResultado = document.getElementById("resultado");
+function agregarAmigo() {
+    if (inputAmigo.value.trim() !== "") {  // Verificar que no esté vacío
+        listaAmigos.push(inputAmigo.value.trim());
+        ulListaAmigos.innerHTML += `<li>${inputAmigo.value.trim()}</li>`;
+        inputAmigo.value = "";  // Limpiar el input después de agregar
+    }
+}
+function sortearAmigo() {
+    if (listaAmigos.length === 0) {
+        ulResultado.innerHTML = `<li>No hay amigos en la lista. Agrega algunos primero.</li>`;
+        return;
+    }
+    const random = Math.floor(Math.random() * listaAmigos.length);
+    const amigoSecreto = listaAmigos[random];
+    ulResultado.innerHTML = `<li>El amigo secreto es: ${amigoSecreto}</li>`;
+}
+
+
+
+
+  
+
+
+
